f5f2c794744823b6247103ef2ae30f03736befa9
Michael Morello <michael.morello@gmail.com>
2014-07-17 13:39:12 +0200

    Update net.c

--- net.c
+++ net.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <libperfstat.h>
 #include "opentsdbc.h"
 
 static struct timeval tv;
 
-int64_t last_poll_ts = 0;
+char * in = " direction=in";
+char * out = " direction=out";
+char * sifname = " ifname=";
 
+char *mktag(char * orig, char * sens, char * ifname) {
 
-perfstat_netinterface_total_t net1;
-
-void net_collect(char * tags) {
-   int i, ret, tot;
-   perfstat_netinterface_total_t net2;
-   char sent[16] = "";
-   char recv[16] = "";
-   gettimeofday(&tv, NULL );
-
-   if (last_poll_ts == 0) {
-        ret = perfstat_netinterface_total(NULL, &net1, sizeof(perfstat_netinterface_total_t), 1);
-	if ( ret > 0 ) {
-		last_poll_ts = tv.tv_sec;	
-	}
-	return;
-   }
-
-   gettimeofday(&tv, NULL );
-   ret = perfstat_netinterface_total(NULL, &net2, sizeof(perfstat_netinterface_total_t), 1);
-   				 
-   if (ret > 0) {
-
-       int elapsed_time = tv.tv_sec - last_poll_ts;
-       last_poll_ts = tv.tv_sec;
-       /* printf("\nTotal Statistics for %d networks\n", net2.number); */
-
-       sprintf(sent, "%8llu", (net2.obytes - net1.obytes) / 1024 / (long) elapsed_time); 
-       sprintf(recv, "%8llu", (net2.ibytes - net1.ibytes) / 1024 / (long) elapsed_time);
-       
-       sendCollectedData(tv.tv_sec, "net.recv", recv, tags);
-       sendCollectedData(tv.tv_sec, "net.sent", sent, tags);
-       
-       memcpy(&net1, &net2, sizeof(perfstat_netinterface_total_t) );
-  }
+	int tagsize = strlen(orig) + strlen(sens) + 1 + strlen(sifname) + strlen(ifname);
+	char* result = malloc(tagsize);
+	sprintf(result, "%s %d %s %s\n", orig, sens, sifname, ifname);
+	return result;
 }
 
+void net_collect(char * tags) {
+	char sent[16] = "";
+	char recv[16] = "";
+
+	perfstat_id_t name;
+	perfstat_netinterface_t *ub;
+	int nnetinterface, i;
+	nnetinterface = perfstat_netinterface(NULL, NULL,
+			sizeof(perfstat_netinterface_t), 0);
+	ub = malloc(sizeof(perfstat_netinterface_t) * nnetinterface);
+	strcpy(name.name, "");
+	if (perfstat_netinterface(&name, ub, sizeof(perfstat_netinterface_t),
+			nnetinterface) >= 0)
+		for (i = 0; i < nnetinterface; i++) {
+			printf("name : %s\n", ub[i].name);
+			printf("\tdescription: %s\n", ub[i].description);
+			printf("\ttype : %u\n", ub[i].type);
+			printf("\tmtu : %llu\n", ub[i].mtu);
+			printf("\tipackets : %llu\n", ub[i].ipackets);
+			printf("\tibytes : %llu\n", ub[i].ibytes);
+			printf("\tierrors : %llu\n", ub[i].ierrors);
+			printf("\topackets : %llu\n", ub[i].opackets);
+			printf("\tobytes : %llu\n", ub[i].obytes);
+			printf("\toerrors : %llu\n", ub[i].oerrors);
+			printf("\tcollisions : %llu\n", ub[i].collisions);
+
+			sprintf(recv, "%8llu", ub[i].ibytes);
+			sprintf(sent, "%8llu", ub[i].obytes);
+
+			char * tagsin = mktags(tags, in, ub[i].name);
+			char * tagsout = mktags(tags, out, ub[i].name);
+
+			sendCollectedData(tv.tv_sec, "proc.net.bytes", recv, tagsin);
+			sendCollectedData(tv.tv_sec, "proc.net.bytes", sent, tagsout);
+
+			free(tagsin);
+			free(tagsout);
+		}
+}
